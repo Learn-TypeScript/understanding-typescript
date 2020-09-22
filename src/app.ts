d88280e5a5cd357ac34ce9474168a0c67610f3b3
fotis tsakiris <footios76@gmail.com>
2020-09-22 20:13:56 +0300

    overriding props and getters and setters

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@
 class Department {
     // a field of the class
     // name: string; 
-    private employees: string[] = [];
+    // private employees: string[] = [];
+    protected employees: string[] = [];
 
     // constructor method
     // readonly exist only in TS
@@ -31,21 +32,49 @@ class ITDepartment extends Department {
     constructor(id: string, admins: string[]) {
         super(id, 'IT');
         this.admins = admins
-
     }
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport () {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.')
+    }
+
+    set mostRecentReport(value: string) {
+        if(!value) {
+            throw new Error('Please pass in a valid value!')
+        }
+        this.addReport(value)
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0]
+    }
+
+    addEmployee(name: string){
+        if (name === 'Max') {
+            return;
+        } 
+        this.employees.push(name)
     }
 
     addReport(text: string) {
         this.reports.push(text)
+        this.lastReport = text;
     }
 
     printReports() {
         console.log(this.reports);
+    }
+
+    printEmployeeInformation() {
+        console.log('Employee Information: ',this.employees.length);
         
     }
 }
@@ -63,8 +92,14 @@ class AccountingDepartment extends Department {
 
  console.log(accounting);
  
+ 
  accounting.addReport('Something went wrong!')
+ accounting.mostRecentReport = 'Year end report'
+ console.log('accounting.mostRecentReport', accounting.mostRecentReport);
  accounting.printReports()
+ accounting.addEmployee('Max')
+ accounting.addEmployee('Fotios')
+ accounting.printEmployeeInformation()
 
 //  const accountingCopy = { name: 'DUMMY', describe: accounting.describe}
 
