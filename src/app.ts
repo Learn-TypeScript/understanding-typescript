38374f4d4492e16054674096e3988871cd6c173d
fotis tsakiris <footios76@gmail.com>
2020-09-22 15:29:21 +0300

    Inheritance

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,8 @@ class Department {
     private employees: string[] = [];
 
     // constructor method
-    constructor(private id: string, public name: string) {
+    // readonly exist only in TS
+    constructor(private readonly id: string, public name: string) {
         // this.name = n;
     }
 
@@ -24,14 +25,46 @@ class Department {
     }
 }
 
- const accounting = new Department('d1','Accounting');
+class ITDepartment extends Department {
+    admins: string[];
 
- accounting.addEmployee('Max');
- accounting.addEmployee('Manu');
-//  accounting.employees[2] = 'Anna'
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins
 
- accounting.describe()
+    }
+}
+
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+    }
+
+    addReport(text: string) {
+        this.reports.push(text)
+    }
+
+    printReports() {
+        console.log(this.reports);
+        
+    }
+}
+
+ const it = new ITDepartment('d1', ['Max']);
+
+ it.addEmployee('Max');
+ it.addEmployee('Manu');
+//  it.employees[2] = 'Anna'
+
+ it.describe()
+ console.log(it);
+
+ const accounting = new AccountingDepartment('d2', [])
+
  console.log(accounting);
+ 
+ accounting.addReport('Something went wrong!')
+ accounting.printReports()
 
 //  const accountingCopy = { name: 'DUMMY', describe: accounting.describe}
 
