bf3cec1ffb0c4bcc527ae6368644ed0ed56c3586
fotis tsakiris <footios76@gmail.com>
2020-09-23 14:17:01 +0300

    type casting correction of v2

--- src/app.ts
+++ src/app.ts
@@ -116,8 +116,10 @@ function moveAnimal(animal: Animal) {
 moveAnimal({type: 'bird', flyingSpeed: 10})
 
 // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+const userInputElement = document.getElementById('user-input') as HTMLInputElement;
 
-if (userInputElement) {
-    (userInputElement as HTMLInputElement).value = 'Hi there';
-}
+// if (userInputElement) {
+//     (userInputElement as HTMLInputElement).value = 'Hi there';
+// }
+
+userInputElement.value = 'Hi there';
