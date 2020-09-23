408e42310baed78e37729a1ae9aebb084aa48054
fotis tsakiris <footios76@gmail.com>
2020-09-23 11:09:22 +0300

    type guard using `in`

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
-interface Admin {
+
+type Admin = {
     name: string;
     privileges: string[];
 }
 
-interface Employee {
+type Employee = {
     name: string;
     startDate: Date;
 }
 
-interface ElevatedEmployee extends Admin, Employee {}
+// interface ElevatedEmployee extends Admin, Employee {}
 
 // combine types with intersection types
-// type ElevatedEmployee = Admin & Employee;
+type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
     name: 'Max',
@@ -25,10 +26,26 @@ type Numeric = number | boolean;
 // describes the types they have in common
 type Universal = Combinable & Numeric
 
-function add(a: Combinable, b: Combinable) {
+function add1(a: Combinable, b: Combinable) {
     // type guard using typeof
     if (typeof a === 'string' || typeof b === 'string') {
         return a.toString() + b.toString()
     } 
     return a + b;
 }
+
+type UnknownenEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownenEmployee) {
+    console.log('Name ' + emp.name);
+    // Problem UnknownEmployee could be a normal Employee that has no privileges.
+    // and we cannot fix it with typeof 
+    if ('privileges' in emp) {
+        console.log('Privileges ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('startDate ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1)
