4d354f9b97e11b36efda4af3e704acea030bbbf3
fotis tsakiris <footios76@gmail.com>
2020-09-24 11:38:26 +0300

    creating a generic function

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 
 // Array is a generic type and requires 1 type argument like eg string.
-const names: Array<string> = [] //  = string[]
-names[0].split(' ')
+// const names: Array<string> = [] //  = string[]
+// names[0].split(' ')
 
-// Give generic type so TS knows what this function will wield.
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout( () => {
-        resolve('This is done!')
-    } , 2000)
-})
+// // Give generic type so TS knows what this function will wield.
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout( () => {
+//         resolve('This is done!')
+//     } , 2000)
+// })
 
-promise.then(data => {
-    data.split(' ');
-})
+// promise.then(data => {
+//     data.split(' ');
+// })
+
+//-----------------------------
+// Creating a generic function:
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+// With generic TS can infer the name and age properties. 
+// const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30})
+const mergedObj = merge({name: 'Max'}, {age: 30})
+console.log(mergedObj.age);
+
+
